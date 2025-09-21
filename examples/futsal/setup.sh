2fc273262661942af7dd433a2f531b5cc8748273
hamanakayuya <c1200988@st.kanazawa-it.ac.jp>
2025-09-21 13:22:31 +0900

    setup.pyのgoogledriveの部分を変更した

--- examples/futsal/setup.sh
+++ examples/futsal/setup.sh
@@ -17,10 +17,9 @@ fi
 # ------------------------------------------------------------
 # 2. download the models  – .pt ウェイト
 #  ※ <xxx> を Google Drive 共有リンクの ID に置き換えてください
-gdown -O "$DIR/data/futsal-ball-detection.pt"     "https://drive.google.com/uc?id=1-F_1yQI5QI3wPNOh0kezGbdgOCKI7KKY"
-gdown -O "$DIR/data/futsal-players-detection.pt"  "https://drive.google.com/uc?id=1ddIqEXFAMozxq9a133Yqfr5XJWrshwaH"
-gdown -O "$DIR/data/futsal-pitch-detection.pt"    "https://drive.google.com/uc?id=1TSByp3LYr_T61hWZU-FqYRo3EvcdYyAt"
-
+gdown -O "$DIR/data/futsal-ball-detection.pt"     "https://drive.google.com/uc?id=1ZYABONNRZ3e_g42HpUPeXSArQ0dAO_gH"
+gdown -O "$DIR/data/best_futsal-players-detection.pt"  "https://drive.google.com/uc?id=12MiU0rUW1GYJu5hfndY6Hh6Ccfi-tIH4"
+gdown -O "$DIR/data/futsal-pitch-detection.pt"    "https://drive.google.com/uc?id=1SpN_FWbnHFEB1gqgkfCpR4dwtcGDK_gn"
 # ------------------------------------------------------------
 # 3. download the videos – テスト用動画
 gdown -O "$DIR/data/test_video.mp4"               "https://drive.google.com/uc?id=1ZXW93E-1mW0lkSME5_oLp_C_o-7wRF70"
